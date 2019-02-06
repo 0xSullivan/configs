46adc55f96e0033da4a470ea83b53514f0db8222
mmd-mostafaee <m.mostafaee@cse.shirazu.ac.ir>
2019-02-06 06:42:44 +0330

    Update post-receive.vue

--- git/hooks/post-receive.vue
+++ git/hooks/post-receive.vue
@@ -1,11 +1,3 @@
-echo "Building assets ... "
 git --work-tree=/home/mmd/website --git-dir=/home/mmd/website.git checkout -f
 git  --work-tree=/home/mmd/website --git-dir=/home/mmd/website.git pull
 echo "Pulling is Done"
-echo "Going to project directory"
-cd /home/mmd/website
-echo "Installing packages ... "
-npm i
-echo "Building assets ... "
-npm run build
-echo "Done"
